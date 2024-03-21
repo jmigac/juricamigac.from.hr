1b6d551fd8e831776b0ab9887dd59f9c226629ae
Jurica Migač <jurica1.migac@gmail.com>
2024-03-21 19:41:19 +0100

    #17 Markdown rendering for descriptions (#18)

--- oneweb/src/components/content/project-article/ProjectArticle.jsx
+++ oneweb/src/components/content/project-article/ProjectArticle.jsx
@@ -1,13 +1,14 @@
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import "../../../styles/components/content/grid/project-article/project-article.css"
+import Markdown from "react-markdown";
 
 function ProjectArticle({project}) {
     return (
         <Row className="project-article">
             <Col>
                 <h4>{project.title}</h4>
-                <p>{project.description}</p>
+                <Markdown>{project.description}</Markdown>
                 <p className="bold flex technology-flex">
                     {project.technologies.map((technology) => <span key={technology} className="technology">{technology}</span>)}
                 </p>
