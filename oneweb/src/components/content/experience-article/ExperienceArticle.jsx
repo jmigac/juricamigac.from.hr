8c2c8912f94ceeb33169195193be8717bb059ebd
Jurica Migac <jurica1.migac@gmail.com>
2024-03-21 19:37:46 +0100

    #17 Markdown rendering for descriptions

--- oneweb/src/components/content/experience-article/ExperienceArticle.jsx
+++ oneweb/src/components/content/experience-article/ExperienceArticle.jsx
@@ -1,9 +1,10 @@
+import Markdown from "react-markdown";
 
 function ExperinceArticle({title, from, until, description}) {
     return (
         <div>
             <h3>{title}</h3>
-            <p>{description}</p>
+            <Markdown>{description}</Markdown>
             <p>{from} - {until}</p>
         </div>
     );
