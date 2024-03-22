177ee5a4eafb3ae367ccedf34083abcbbe37f6e4
Jurica Migac <jurica1.migac@gmail.com>
2024-03-22 18:03:24 +0100

    Blog refactoring

---
+++ oneweb/src/pages/OneWeb.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/global.css'
+import Header from "../components/structure/header/header/Header";
+import Body from "../components/structure/body/Body"
+import Footer from '../components/structure/footer/footer/Footer';
+
+function OneWeb() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+
+export default OneWeb;
