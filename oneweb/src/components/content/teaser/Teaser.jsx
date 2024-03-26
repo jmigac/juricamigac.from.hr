b6df61cc1b7b014361843f0002d76779418c91e1
Jurica Migač <jurica1.migac@gmail.com>
2024-03-26 21:06:37 +0100

    Initial lighthouse improvements (#24)
    * Initial lighthouse improvements
    
    * Setting bigger max title
    
    * Testing with added srcSet
    
    * Removing srcSet

--- oneweb/src/components/content/teaser/Teaser.jsx
+++ oneweb/src/components/content/teaser/Teaser.jsx
@@ -1,14 +1,42 @@
+import React from "react";
 import "../../../styles/components/content/teaser/teaser.css"
 
-function Teaser({imageSource, altText, alignment}) {
-    return (
-        <div className={'teaser-' + alignment}>
-            <img src={imageSource} 
-                 alt={altText} 
-                 loading="eager"
-                 className="teaser" /> 
-        </div>
-    );
+class Teaser extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.imageSource = props.imageSource;
+        this.altText = props.altText;
+        this.alignment = props.alignment;
+        this.state = {
+            image: null
+        }
+    }
+
+    componentDidMount() {
+        const image = new Image();
+        image.src = this.imageSource;
+        this.setState({ image: image });
+    }
+
+    areWidthAndHeightPresent() {
+        return this.state.image?.width !== 0 && this.state.image?.height !== 0
+    }
+
+    render() {
+        if (this.state.image != null && this.areWidthAndHeightPresent()) {
+            return (
+                <div className={'teaser-' + this.alignment}>
+                    <img src={this.imageSource} 
+                        alt={this.altText} 
+                        loading="lazy"
+                        className="teaser"
+                        width={this.state.image?.width}
+                        height={this.state.image?.height} /> 
+                </div>
+            );
+        }
+    }
 }
 
 export default Teaser;
