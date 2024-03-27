2d4a12e1408e288269bde608376ad89f9047ea89
Jurica Migac <jurica1.migac@gmail.com>
2024-03-27 13:51:56 +0100

    Removing Contentful API calls from React

--- oneweb/src/services/Contentful.jsx
+++ oneweb/src/services/Contentful.jsx
@@ -1,37 +1,13 @@
 import axios from 'axios'
 
-const spaceId = process.env.REACT_APP_SPACE_ID;
-const environment = process.env.REACT_APP_ENVIRONMENT;
-const token = process.env.REACT_APP_TOKEN;
-const api = `https://graphql.contentful.com/content/v1/spaces/${spaceId}/environments/${environment}`;
-
 const FUNCTION_APIS = {
     PROJECTS: "https://cache-function.juricamigac.from.hr/projects",
     EXPERIENCES: "https://cache-function.juricamigac.from.hr/experiences"
 }
 
-function getRequestData(query) {
-    let data = JSON.stringify({
-        query: query,
-        variables: {}
-      });
-      
-      let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: api,
-        headers: { 
-          'Content-Type': 'application/json', 
-          'Authorization': `Bearer ${token}`
-        },
-        data : data
-      };
-      return config;
-}
-
  const Contentful = {
 
-    getExperienceArticles : async (query) => {
+    getExperienceArticles : async () => {
         try {
             const { data: response } = await axios.get(FUNCTION_APIS.EXPERIENCES);
             return response;
@@ -40,7 +16,7 @@ function getRequestData(query) {
         }
     },
     
-    getProjectArticles : async (query) => {
+    getProjectArticles : async () => {
         try {
             const { data: response } = await axios.get(FUNCTION_APIS.PROJECTS);
             return response;
