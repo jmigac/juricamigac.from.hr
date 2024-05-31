b8745e30d781162d0c0efa5c26ba5c272d2ef4c9
Jurica Migač <jurica1.migac@gmail.com>
2024-05-31 15:09:27 +0200

    Background and shadow changes (#31)
    * Glucose style changes
    
    * Background change of glucose readings

--- oneweb/src/pages/GlucosePage.jsx
+++ oneweb/src/pages/GlucosePage.jsx
@@ -1,8 +1,9 @@
 import GlucoseLevel from "../components/structure/header/glucose-level/GlucoseLevel";
+import "../styles/pages/glucose-page/glucose.css"
 
 function GlucosePage() {
     return (
-        <div>
+        <div className="glucose-page">
             <GlucoseLevel />
         </div>
     );
