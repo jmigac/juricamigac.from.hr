c4499e47cda020e3526763eb2340e9efeaf25536
Jurica Migač <jurica1.migac@gmail.com>
2024-08-16 18:48:36 +0200

    Footer Social media links (#48)

--- oneweb/src/components/structure/footer/footer/Footer.jsx
+++ oneweb/src/components/structure/footer/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import Copyright from "../copyright/Copyright";
 import "../../../../styles/components/structure/footer/footer/footer.css"
+import SocialMediaLinks from "../social-media-links/SocialMediaLinks";
 
 function Footer() {
     return (
         <footer className="footer">
             <Copyright />
+            <SocialMediaLinks />
         </footer>
     );
 }
