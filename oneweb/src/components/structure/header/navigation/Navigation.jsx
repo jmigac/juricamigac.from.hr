f3eb3f063634d167b0ccb608132b532d69e7bf33
Jurica Migac <jurica1.migac@gmail.com>
2025-02-21 21:49:04 +0100

    Google Analytics

--- oneweb/src/components/structure/header/navigation/Navigation.jsx
+++ oneweb/src/components/structure/header/navigation/Navigation.jsx
@@ -1,6 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import ReactGA from "react-ga4";
+
+const handleNavigation = (url, name) => {
+    ReactGA.event({
+        category: 'Navigation',
+        action: 'visited',
+        label: name,
+    });
+}
 
 export default function Navigation() {
     return (
@@ -11,7 +20,7 @@ export default function Navigation() {
                     <Nav.Link href="#experiences">Experiences</Nav.Link>
                     <Nav.Link href="#projects">Projects</Nav.Link>
                     <Nav.Link href="#expertise">Expertise</Nav.Link>
-                    <Nav.Link href="https://blog.juricamigac.from.hr">Blog</Nav.Link>
+                    <Nav.Link href="https://blog.juricamigac.from.hr" onClick={() => handleNavigation("https://blog.juricamigac.from.hr", "Blog")}>Blog</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
