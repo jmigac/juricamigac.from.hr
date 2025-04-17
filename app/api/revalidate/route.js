8c9b69c9e76bddcc0a2adb87a0b3c232eebcda81
Jurica Migač <jurica1.migac@gmail.com>
2025-04-17 21:58:49 +0200

    Content invalidation (#66)

---
+++ app/api/revalidate/route.js
@@ -0,0 +1,13 @@
+import {NextResponse} from "next/server";
+import {revalidatePath} from "next/cache";
+
+export async function POST(request) {
+    const requestHeaders = new Headers(request.headers);
+    const secret = requestHeaders.get('x-vercel-revalidate-key');
+    if (secret !== process.env.REVALIDATION_SECRET_KEY) {
+            return NextResponse.json({message: 'Invalid secret'}, {status: 401}
+        );
+    }
+    revalidatePath('/');
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+}
