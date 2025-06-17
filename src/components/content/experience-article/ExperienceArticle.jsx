3ca915ddc00352374019d5863e2a76aa6abfe615
Jurica Migač <jurica.migac@cyber64.com>
2025-06-17 10:27:10 +0200

    Changing styles for dark themes

--- src/components/content/experience-article/ExperienceArticle.jsx
+++ src/components/content/experience-article/ExperienceArticle.jsx
@@ -8,7 +8,7 @@ export default function ExperienceArticle({ experience }) {
             <div
                 className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">{from}</time>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{experience.title}</h3>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-black">{experience.title}</h3>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{experience.description}</p>
         </li>
     );
